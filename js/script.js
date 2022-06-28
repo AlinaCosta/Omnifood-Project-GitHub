7c59ad406fd21bd4edd9ffd1f300bbcee74e4a3d
Alina Costa <alinacosta87@gmail.com>
2022-06-28 18:07:22 +0300

    created smooth scrolling animation with javascript (Scroll back to top, scroll to other links) and i can close the mobile navigation when i click on a link

--- js/script.js
+++ js/script.js
@@ -26,7 +26,28 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+///////////////////////////////////////////////////////
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
+    //Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation when you click on a link
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 /////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
